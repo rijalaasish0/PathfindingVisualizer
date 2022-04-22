08256637423513a740c4eb52d3c128cf85d1befb
rij0 <rijalaasish0@gmail.com>
2022-04-21 19:11:30 -0500

    Implemented Vertex

---
+++ src/Components/Vertex.js
@@ -0,0 +1,17 @@
+const Vertex = ({colNum, isFinish, isStart, isWall, onMouseDown, onMouseEnter, onMouseUp, rowNum}) => {
+    const vertexClassName = 
+        isFinish ? 'vertex-finish' :
+        isStart ? 'vertex-start' :
+        isWall ? 'vertex-wall' :
+        '';
+    
+        return (
+            <td
+                id={`vertex-${rowNum}-${colNum}`}
+                className={`vertex ${vertexClassName}`}
+                
+            >
+            
+            </td>
+        )
+}
