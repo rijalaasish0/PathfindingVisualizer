42295e8db7244468c1b7f3d2e77993ee4def35ee
rij0 <rijalaasish0@gmail.com>
2022-04-21 23:22:51 -0500

    Minor changes

--- src/Components/PathfindingVisualizer.js
+++ src/Components/PathfindingVisualizer.js
@@ -1,14 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Vertex from './Vertex';
 import './PathfindingVisualizer.css'
 
 const PathfindingVisualizer = () => {
     const [grid, setGrid] = useState([]);
-    const [START_VERTEX_ROW, setStartVertexRow] = useState(0);
-    const [FINISH_Vertex_ROW, setFinishVertexRow] = useState(10);
-    const [START_Vertex_COL, setStartVertexCol] = useState(10);
-    const [FINISH_Vertex_COL, setFinishVertexCol] = useState(0);
-    const [row_count, setRowCount] = useState(20);
+    const [start_vertex_row, setStartVertexRow] = useState(4);
+    const [finish_vertex_row, setFinishVertexRow] = useState(10);
+    const [start_vertex_col, setStartVertexCol] = useState(10);
+    const [finish_vertex_col, setFinishVertexCol] = useState(19);
+    const [row_count, setRowCount] = useState(15);
     const [col_count, setColCount] = useState(20);
     const [isStartVertex, setIsStartVertex] = useState(false);
     const [isFinishVertex, setIsFinishVertex] = useState(false);
@@ -26,11 +26,11 @@ const PathfindingVisualizer = () => {
     const getInitialGrid = (rowCount = row_count, colCount = col_count) => {
         const initialGrid = [];
         for (let row = 0; row < rowCount; row++) {
-          const currentRow = [];
-          for (let col = 0; col < colCount; col++) {
-            currentRow.push(createVertex(row, col));
-          }
-          initialGrid.push(currentRow);
+            const currentRow = [];
+            for (let col = 0; col < colCount; col++) {
+                currentRow.push(createVertex(row, col));
+            }
+            initialGrid.push(currentRow);
         }
         return initialGrid;
     }
@@ -40,19 +40,19 @@ const PathfindingVisualizer = () => {
     }
 
     const createVertex = (row, col) => {
-        return{
-            row, 
-            col, 
+        return {
+            row,
+            col,
             isStart:
-                row === START_VERTEX_ROW && 
-                col === START_Vertex_COL,
+                row === start_vertex_row &&
+                col === start_vertex_col,
             isFinish:
-                row === FINISH_Vertex_ROW &&
-                col === FINISH_Vertex_COL,
+                row === finish_vertex_row &&
+                col === finish_vertex_col,
             distance: Infinity,
             distanceToFinishVertex:
-                Math.abs(FINISH_Vertex_ROW - row) +
-                Math.abs(FINISH_Vertex_COL - col),
+                Math.abs(finish_vertex_row - row) +
+                Math.abs(finish_vertex_col - col),
             isVisited: false,
             isWall: false,
             previousNode: null,
@@ -60,33 +60,33 @@ const PathfindingVisualizer = () => {
         }
     }
 
-    return(
+    return (
         <div>
             <h2>Pathfinding Visualizer</h2>
             <table
                 className="grid-container">
-                    <tbody className = "grid">
-                        {grid.map((row, rowIdx) => {
-                            return(
-                                <tr key={rowIdx}>
-                                    {row.map((vertex, vertexIdx) => {
-                                        const {row, col, isFinish, isStart, isWall} = vertex;
-                                        return(
-                                            <Vertex
-                                                key={vertexIdx}
-                                                col={col}
-                                                row={row}
-                                                isFinish={isFinish}
-                                                isStart={isStart}
-                                                isWall={isWall}
-                                            ></Vertex>    
-                                        );
-                                    })}
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
+                <tbody className="grid">
+                    {grid.map((row, rowIdx) => {
+                        return (
+                            <tr key={rowIdx}>
+                                {row.map((vertex, vertexIdx) => {
+                                    const { row, col, isFinish, isStart, isWall } = vertex;
+                                    return (
+                                        <Vertex
+                                            key={vertexIdx}
+                                            col={col}
+                                            row={row}
+                                            isFinish={isFinish}
+                                            isStart={isStart}
+                                            isWall={isWall}
+                                        ></Vertex>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
 
     );
