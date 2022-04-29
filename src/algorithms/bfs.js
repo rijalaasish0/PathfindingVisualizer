5f1a0e195eb06351f382093e6f85b847d5119bb8
ALambert129 <albertvlambert@gmail.com>
2022-04-28 22:34:01 -0500

    Algorithms Galore
    Added buttons to the main pathfinding script for BFS and DFS
    
    Created the BFS and DFS scripts

--- src/algorithms/bfs.js
+++ src/algorithms/bfs.js
@@ -1,3 +1,56 @@
 export function BFS(grid, startVertex, finishVertex){
-    
+    //List of the visited vertices part of the path
+    const visitedVerticesInOrder = [];
+
+    //The first node we explore is the Starting Vertex
+    let nextVerticesStack = [startVertex];
+    //For this case, we are using an array, but
+    //we're going to make it act like a queue
+    //array.shift() pops the item off the top of the array 
+    //like you would a queue, as you'll see later
+
+
+    while(nextVerticesStack.length){
+        //remove the first item of the queue
+        const currentVertex = nextNodesStack.shift();
+        //return the path if we reached the finish vertex
+        if(currentVertex === finishVertex)
+            return visitedNodesInOrder;
+        
+        //otherwise, continue searching the graph
+        if(!currentVertex.isWall && (currentVertex.isStart || !currentVertex.isVisited)){
+            currentVertex.isVisited = true;
+            visitedVerticesInOrder.push(currentVertex);
+            const {col, row} = currentVertex;
+            let nextVertex;
+            if(row > 0){
+                nextVertex = grid[row -1][col];
+                if(!nextVertex.isVisited){
+                    nextVertex.previousVertex = currentVertex;
+                    nextVerticesStack.push(nextVertex);
+                }
+            }
+            if(row < grid.length - 1){
+                nextVertex = grid[row + 1][col];
+                if(!nextVertex.isVisited){
+                    nextVertex.previousVertex = currentVertex;
+                    nextVerticesStack.push(nextVertex);
+                }
+            }
+            if(col > 0){
+                nextVertex = grid[row][col -1];
+                if(!nextVertex.isVisited) {
+                    nextVertex.previousVertex = currentVertex;
+                    nextVerticesStack.push(nextVertex);
+                }
+            }
+            if(col < grid[0].length - 1){
+                nextVertex = grid[row][col + 1];
+                if(!nextVertex.isVisited){
+                    nextVertex.previousVertex = currentVertex;
+                    nextVerticesStack.push(nextVertex);
+                }
+            }
+        }
+    }
 }
---
+++ src/algorithms/dfs.js
@@ -0,0 +1,48 @@
+export function DFS(grid, startVertex, finishVertex){
+    const visitedVerticesInOrder = [];
+    const nextVerticesStack = [startVertex];
+    while(nextVerticesStack.length){
+        const currentVertex = nextVerticesStack.pop();
+
+        if(currentVertex === finishNode) {
+            return visitedVerticesInOrder;
+        }
+
+        if(!currentVertex.isWall &&
+            (currentVertex.isStart || !currentVertex.isVisited)){
+            currentVertex.isVisited = true;
+            visitedVerticesInOrder.push(currentVertex);
+
+            const {col, row} = currentVertex;
+            let nextVertex;
+            if(row > 0){
+                nextVertex = grid[row - 1][col];
+                if(!nextVertex.isVisited){
+                    nextVertex.previousVertex = currentVertex;
+                    nextVerticesStack.push(nextVertex);
+                }
+            }
+            if(row < grid.length - 1){
+                nextVertex = grid[row + 1][col];
+                if(!nextVertex.isVisited){
+                    nextVertex.previousVertex = currentVertex;
+                    nextVerticesStack.push(nextVertex);
+                }
+            }
+            if(col < grid[0].length - 1){
+                nextVertex = grid[row][col + 1];
+                if(!nextVertex.isVisited){
+                    nextVertex.previousVertex = currentVertex;
+                    nextVerticesStack.push(nextVertex);
+                }
+            }
+            if(col > 0){
+                nextVertex = grid[row][col - 1];
+                if(!nextVertex.isVisited){
+                    nextVertex.previousVertex = currentVertex;
+                    nextVerticesStack.push(nextVertex);
+                }
+            }
+        }
+    }
+}
